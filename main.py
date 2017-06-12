f5cf8c5353983b6bd50ceb3cbac25b6ffba9903c
dchandlerp <chandler.prince@gmail.com>
2017-06-12 16:24:13 -0500

    Finished User-Signup

--- main.py
+++ main.py
@@ -15,22 +15,31 @@ def validate():
     password = request.form["password"]
     verify = request.form["verify"]
     email = request.form["email"]
-    if is_username_valid(username):
-        pass
-    else:
-        pass
-    if does_pw_match(password):
-        pass
-    else:
-        pass
-    if is_email_valid(email):
-        pass
-    else:
-        pass
+    username_error = ""
+    password_error = ""
+    email_error = ""
+
+    if not is_username_valid(username):
+        username_error = "Invalid Username"
+        username = ""
+    if not does_pw_match(password, verify):
+        password_error = "Passwords didn't match"
+        password = ""
+        verify = ""
+    if not email:
+        pass    
+    elif not is_email_valid(email):
+            email_error = "Invalid Email Address"
+            email = ""
     
+    if not username_error and not password_error and not email_error:
+        return render_template("welcome.html", username = username)
+    else:
+        return render_template('index.html', username_error=username_error,
+            password_error=password_error, email_error= email_error,
+            username = username, password = password, email = email,
+            verify = verify)
     
-@app.route("/welcome")
-    return render_template("welcome.html")
 
 def does_pw_match(password, verify):
     return password == verify
@@ -40,6 +49,5 @@ def is_email_valid(email):
 
 def is_username_valid(username):
     return re.match(r'^[\S]{3,20}$', username) is not None
-
 
 app.run()
